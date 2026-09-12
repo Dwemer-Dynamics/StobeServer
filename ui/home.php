3edbdd08e77cf335629acc7dd89a3dd6b0c089e2
RANGROO <reecemeakings@gmail.com>
2026-09-12 16:04:37 -0700

    Bump StobeServer version to 1.3.0

--- ui/home.php
+++ ui/home.php
@@ -225,11 +225,11 @@ foreach ($versionCandidates as $versionPath) {
     }
 }
 if ($serverVersionDisplay === '') {
-    $serverVersionDisplay = '1.2.3';
+    $serverVersionDisplay = '1.3.0';
 }
 $serverReleaseDate = readVersionFile(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'release_date.txt');
 if ($serverReleaseDate === '') {
-    $serverReleaseDate = '2026-08-31';
+    $serverReleaseDate = '2026-09-12';
 }
 
 $pluginVersionDisplay = 'N/A';
